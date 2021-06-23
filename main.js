0a20edbe4d8d59d0f1a94b2e9168a18baa2ea1aa
Raghav-Saxena22 <74664397+Raghav-Saxena22@users.noreply.github.com>
2021-06-23 12:45:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+
+
+
+
+function bedroom()
+{
+    window.location = "bedroom.html"
+}
+
+function back()
+{
+    window.location = "index.html"
+}
+
+function tv_ac()
+{
+    window.location = "tv_ac.html"
+}
+
+function desk()
+{
+    window.location = "desk.html"
+}
+
+function bottles()
+{
+    window.location = "bottles.html"
+}
+
+function fruit()
+{
+    window.location = "bottles.html"
+}
